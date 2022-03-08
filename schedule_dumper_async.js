c7110dadbd22752cd2da725369f85a642298a7d5
Tao, Xiangkun | Shawn | RP <xiangkun.tao@rakuten.com>
2022-03-08 10:33:42 +0900

    update

--- schedule_dumper_async.js
+++ schedule_dumper_async.js
@@ -7,8 +7,8 @@
 // @include      https://syllabus.naist.jp/subjects/preview_detail/*
 // @include      https://syllabus.naist.jp/charges/preview_detail/*
 // @grant        GM.xmlHttpRequest
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js
 // @run-at       context-menu
 // @connect      syllabus.naist.jp
 // ==/UserScript==
